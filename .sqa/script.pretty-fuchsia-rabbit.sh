0d9a5725f71974d2b3a0385e2c63123932d2b146
orviz <orviz@ifca.unican.es>
2024-02-23 15:22:10 +0100

    Add JePL file structure

--- .sqa/script.pretty-fuchsia-rabbit.sh
+++ .sqa/script.pretty-fuchsia-rabbit.sh
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
-cd github.com/m-team-kit/mlflow-auth-gui &&
-    get_git_tags.py
+cd codebase.helmholtz.cloud/m-team/ai/mlflow-auth-gui &&
+    find_doc_files.py --file_type all
 )
